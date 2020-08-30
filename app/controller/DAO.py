b80cde58e10609d977fdc5bfd4d6f7620f9495a8
Pedro Sousa <ppls2106@gmail.com>
2020-08-30 13:05:27 -0300

    add generic DAO

---
+++ app/controller/DAO.py
@@ -0,0 +1,37 @@
+from app import db
+
+
+def transacao(objeto):
+    db.session.add(objeto)
+    db.session.commit()
+    db.session.close()
+
+
+def buscar_por_criterio(table, **filtros):
+    return table.query.filter_by(**filtros).first()
+
+
+def buscar_por_criterio_bool(table, *filtros):
+    return table.query.filter(*filtros).first()
+
+
+def busca_join_composto_com_criterio(table1, table2, table3, *filtro):
+    return table1.query.join(table2).join(table3).filter(*filtro).add_entity(table2).add_entity(table3).first()
+
+
+def buscar_por_join(table1, table2, *filtro):
+    return table1.query.join(table2).filter(*filtro).add_entity(table2).first()
+
+
+def buscar_todos(table, *order_by):
+    return table.query.order_by(*order_by).all()
+
+
+def buscar_todos_por_criterio(table, **filtros):
+    return table.query.filter_by(**filtros).all()
+
+
+def deletar(objeto):
+    db.session.delete(objeto)
+    db.session.commit()
+    db.session.close()

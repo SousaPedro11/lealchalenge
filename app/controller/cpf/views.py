8c76f96a90aa8db207949aae4a1b87fb2511238a
Pedro Sousa <ppls2106@gmail.com>
2020-08-31 02:22:02 -0300

    add cpf validator form and modified view

--- app/controller/cpf/views.py
+++ app/controller/cpf/views.py
@@ -1,8 +1,15 @@
 from flask import render_template
 
 from app.controller.cpf import cpf_bp
+from app.controller.cpf.forms import CPFForm
+from .cpf_validator import validar
 
 
-@cpf_bp.route('/cpf/')
+@cpf_bp.route('/cpf/', methods=['GET', 'POST'])
 def validador():
-    return render_template('cpf.html')
+    form = CPFForm()
+    retorno = ''
+    if form.validate_on_submit():
+        retorno = 'CPF válido' if validar(form.string.data) else 'CPF Inválido'
+
+    return render_template('cpf.html', form=form, retorno=retorno)

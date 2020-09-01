112dd452057e8cca4cebc5d810ab75a60d51d3eb
Pedro Sousa <ppls2106@gmail.com>
2020-09-01 17:11:23 -0300

    add sql code route

--- app/controller/endereco/views.py
+++ app/controller/endereco/views.py
@@ -1,6 +1,18 @@
+from flask import render_template
+
 from app.controller.endereco import endereco_bp
 
 
 @endereco_bp.route('/enderecos/', methods=['GET', 'POST', 'UPDATE', 'DELETE'])
 def enderecos():
     pass
+
+
+@endereco_bp.route('/database/')
+def database():
+    with open('app/sql/DDL.sql') as f:
+        ddl = '\n'.join([i.replace('\n', '') for i in f.readlines()])
+    with open('app/sql/DML.sql') as f:
+        dml = '\n'.join([i.replace('\n', '') for i in f.readlines()])
+
+    return render_template('sql.html', ddl=ddl, dml=dml, dql='dql')

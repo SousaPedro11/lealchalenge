87402871a48cb1b019c39893d39cd75b30cb78e4
Pedro Sousa <ppls2106@gmail.com>
2020-09-06 18:06:54 -0300

    update config to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+from app.controller.DAO import root_db
 from app.controller.cep import cep_bp
 from app.controller.challenge import challenge_bp
 from app.controller.cpf import cpf_bp
-from app.controller.endereco import endereco_bp
+from app.controller.endereco import endereco_bp, db
 from config import Config
 
-db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
@@ -16,11 +17,16 @@ def create_app():
     app.config.from_object(Config)
     Bootstrap(app=app)
 
+    db.init_app(app)
+    root_db.init_app(app)
+    migrate.init_app(app=app, db=db)
+
     app.register_blueprint(cep_bp)
     app.register_blueprint(challenge_bp)
     app.register_blueprint(cpf_bp)
     app.register_blueprint(endereco_bp)
 
-    db.init_app(app=app)
+    with app.app_context():
+        db.create_all()
 
     return app
